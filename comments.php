5a529b3f6b0eea91b233cc07f75a1ab39c587fad
Prabhvir Babra <theprabab@gmail.com>
2023-08-11 08:25:36 -0700

    Style comments

--- comments.php
+++ comments.php
@@ -2,15 +2,20 @@
 
 <?php
 $comment_form_args = array(
-    'label_submit' => 'Send',
+    'label_submit' => '↲',
     'title_reply' => 'Write a Comment',
     'comments_notes_after' => '',
-    'comment_field' => '<p class="comment-form-comment"><label for="comment">' . _x( 'Comment', 'noun' ) . '</label><br /><textarea id="comment" name="comment" aria-required="true"></textarea></p>',
+    'comment_field' => '<p class="comment-form-comment"><textarea id="comment" name="comment" aria-required="true"></textarea></p>',
     'must_log_in' => 'You must login before commenting on a comic.',
+    'logged_in_as' => '',
 );
 
 comment_form( $comment_form_args );
+?>
 
+<h3 id="discussion-title"> Discussion </h3>
+
+<?php
 $comments = wp_list_comments();
 
 // Comment Loop
@@ -19,6 +24,5 @@ if ( $comments ) {
 		echo '<p>' . $comment->comment_content . '</p>';
 	}
 } else {
-	echo 'No comments found.';
 }
 ?>
